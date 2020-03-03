209b9099d283930f7052a6f287bd97ea1d3c61c7
Dasha Akhten <dasha@Dashas-iMac.attlocal.net>
2020-03-03 14:06:29 -0800

    css for participants list

--- src/ParticipantsList.js
+++ src/ParticipantsList.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { participants } from "./participantsStore";
+import "./participantsList.css";
 
 export default function displayParticipants(props) {
   let session, stage;
-  if(props.inSession) {
-    session = <p>In session</p>
+  if (props.inSession) {
+    session = <p>In session</p>;
   } else {
-    session = <p>Left</p>
+    session = <p>Left</p>;
   }
-  if(props.onStage) {
-    stage = <p>On stage</p>
+  if (props.onStage) {
+    stage = <p>On stage</p>;
   }
   return (
-          <li >
-            {props.id} {props.name}
-           <img src={props.avatar} /> 
-           {session} {stage}
-          </li>
+    <li>
+      {props.id} {props.name}
+      <img src={props.avatar} />
+      {session} {stage}
+    </li>
   );
 }
